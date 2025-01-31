9479286b09ab838341ea666683fc9f66ba350347
BrJuhnior <mauriciojuhnior88@gmail.com>
2025-01-30 21:27:33 -0300

    Add files via upload

---
+++ Atividade S,A/Trabalho S.A/Bebidas.js
@@ -0,0 +1,8 @@
+let anoNascimento
+let resultado
+anoNascimento =  Number(prompt("Digite o ano que você nasceu?"))
+if(resultado >= 18){
+    resultado = 2025 - anoNascimento 
+}else{
+    alert(" Você não é de maior!!!")
+}
